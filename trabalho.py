820210fe2fa001faec87bcda31c0e7b8bb7c5b7c
vini-kenzo <vkf5@hotmail.com>
2021-03-30 17:30:44 -0300

    commit inicial

---
+++ trabalho.py
@@ -0,0 +1,29 @@
+# Arquitetura e Organização de Computadores I
+# Trabalho 1
+# Implementação de simulador para uma arquitetura com pipeline simplificada
+#
+# Aluno: Vinícius Kenzo Fukace
+# RA: 115672
+
+from typing import List, Dict, Tuple
+from collections import deque
+
+
+# Definição de Dados
+
+class Vertice:
+    def __init__(self) -> None:
+        self.d: int = None
+        self.f: int = None
+        self.pai: str = None
+        self.cor: str = None
+        self.adj: List[int] = []
+
+
+def main():
+    a = 0
+    exit
+
+
+if __name__ == '__main__':
+    main()
